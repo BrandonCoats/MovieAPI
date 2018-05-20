55f0f2a214f7eac5f7f52c97dc3d3c4120ac70ef
Brandon Coats <bgcoats1313@gmail.com>
2018-05-20 15:18:00 -0600

    Pre sql-post routes

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,13 +1,17 @@
 package Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages="Controllers")
+@EntityScan(basePackages="Models")
+@EnableJpaRepositories(basePackages="Repos")
 public class Main extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
